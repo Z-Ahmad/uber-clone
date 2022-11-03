2145762e5bfda9890fcf04b1bca020bcba3ef645
Zaki Ahmad <zakiahmad42661@gmail.com>
2022-11-03 15:15:24 -0400

    basic navbar information added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,25 @@ import Link from "next/link";
 function Navbar(){
   return(
     <header>
-      <div className="flex items-center space-x-2 md:space-x-10"></div>
+      <div className="flex items-center space-x-2 md:space-x-10">
+
       <img src="/images/uber.svg" alt="" width={50} height={50}/>
+
+      <ul className="hidden space-x-4 md:flex">
+        <li className="navLink">Company</li>
+        <li className="navLink">Safety</li>
+        <li className="navLink">Help</li>
+        <li className="navLink">Products</li>
+      </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <button>Log In</button>
+        <button>Sign Up</button>
+      </div>
     </header>
   )
 }
+
 
 export default Navbar
