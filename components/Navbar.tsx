7ce13b1a216085587036eea0b0984706b9a900d1
Zaki Ahmad <zakiahmad42661@gmail.com>
2022-11-03 16:34:35 -0400

    fixed alignment of navbar elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,26 +2,32 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 
 function Navbar(){
-  return(
-    <header>
+  return (
+    <header className="flex justify-between px-4 bg-black text-white">
       <div className="flex items-center space-x-2 md:space-x-10">
-
-      <img src="/images/uber.svg" alt="" width={50} height={50}/>
-
-      <ul className="hidden space-x-4 md:flex">
-        <li className="navLink">Company</li>
-        <li className="navLink">Safety</li>
-        <li className="navLink">Help</li>
-        <li className="navLink">Products</li>
-      </ul>
+        <img  
+        className="cursor-pointer py-2" 
+        src="/images/uber.svg" 
+        alt="" 
+        width={40} 
+        height={40}
+        style={{fill:'#FFFFFF'}}
+        />
+        
+        <ul className="hidden space-x-4 md:flex">
+          <li>Company</li>
+          <li>Safety</li>
+          <li>Help</li>
+          <li>Products</li>
+        </ul>
       </div>
 
-      <div className="flex items-center space-x-4 text-sm font-light">
+      <div className="flex items-center space-x-4 text-sm font-light ml-10">
         <button>Log In</button>
         <button>Sign Up</button>
       </div>
     </header>
-  )
+  );
 }
 
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Uber</h1>
+      {/* <h1>Uber</h1>
       <div className='flex'>
         <div>Drive or deliver</div>
         <div>Eat</div>
         <div>Ride</div>
-      </div>
+      </div> */}
       
     </div>
   )
